f86548df27ce45aecd9c97507c080448eba878d3
hanabi1224 <harlowmoo@gmail.com>
2024-08-14 19:16:43 +0800

    basic CI setup (#5)

--- blssig/src/lib.rs
+++ blssig/src/lib.rs
@@ -1,3 +1,6 @@
+// Copyright 2019-2024 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
--- gpbft/src/lib.rs
+++ gpbft/src/lib.rs
@@ -1,3 +1,6 @@
+// Copyright 2019-2024 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
 mod types;
 
 pub use types::PubKey;
